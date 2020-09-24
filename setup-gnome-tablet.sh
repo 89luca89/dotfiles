0c392efbf72335d2352cdb86119926fdabcf3477
89luca89 <luca.dimaio1@gmail.com>
2020-09-24 16:16:39 +0200

    Work on setup for gnome tablets

--- setup-gnome-tablet.sh
+++ setup-gnome-tablet.sh
@@ -1,4 +1,16 @@
-sudo dnf install -y onboard gnome-extensions-app gnome-tweaks
+sudo dnf install -y \
+    cheese \
+    geary \
+    gnome-calendar \
+    gnome-clocks \
+    gnome-contacts \
+    gnome-extensions-app \
+    gnome-music \
+    gnome-tweaks \
+    gnome-weather \
+    marker \
+    onboard \
+    totem
 
 echo '[Desktop Entry]
 Name=Fix Touch
@@ -20,13 +32,27 @@ Comment=Rotate system
 Exec=/home/luca-linux/bin/rotate.sh
 Terminal=true
 Type=Application
-Icon=system-reboot
+Icon=rotation-allowed-symbolic
 Categories=GTK;Utility;
 Keywords=Start again;Rotate;
 StartupNotify=true' | tee ~/.local/share/applications/rotate.desktop
 
+echo 'Section "Device"
+  Identifier "Intel Graphics"
+  Driver "intel"
+
+  Option "TearFree" "true"
+EndSection' | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
+
 pushd ~/Syncthing/Conf
 for ext in *extension.zip; do
-    gnome-extensions install $ext --force
+	gnome-extensions install $ext --force
 done
 popd
+
+gsettings set org.onboard theme '/usr/share/onboard/themes/Droid.theme'
+gsettings set org.onboard layout '/usr/share/onboard/layouts/Compact.onboard' #Phone.onboard
+gsettings set org.onboard.auto-show enabled true
+gsettings set org.onboard.keyboard audio-feedback-enabled true
+gsettings set org.onboard.keyboard touch-feedback-enabled true
+gsettings set org.onboard.window docking-enabled true
