99f75e667d5be16268b37e3f21c0100e737414aa
89luca89 <luca.dimaio1@gmail.com>
2020-12-12 14:19:58 +0100

    formatting

--- bin/get_chromium_tabs.sh
+++ bin/get_chromium_tabs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Get list of open tabs from chromium browser
 
-strings ~/.config/chromium/Default/Current\ Session | grep -Eo '(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'  | grep -v '/$' | grep -v '=$' | sort -u
+strings ~/.config/chromium/Default/Current\ Session | grep -Eo '(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' | grep -v '/$' | grep -v '=$' | sort -u
