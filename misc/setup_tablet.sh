9f830e2d25bd473c3c5d152de8df56a0a49acfe2
89luca89 <luca.dimaio1@gmail.com>
2020-12-24 18:23:07 +0100

    Tablet Setup update

--- misc/setup_tablet.sh
+++ misc/setup_tablet.sh
@@ -3,37 +3,19 @@
 set -o errexit
 set -o nounset
 
-sudo usermod -aG luca-linux gdm
-sudo chmod 0755 /var/lib/gdm/.config/
-sudo chmod g+w /var/lib/gdm/.config/
-sudo chmod g+w /var/lib/gdm/.config/monitors.xml
-
-sudo dnf install -y \
-	cheese \
-	geary \
-	gnome-calendar \
-	gnome-clocks \
-	gnome-contacts \
-	gnome-extensions-app \
-	gnome-music \
-	gnome-tweaks \
-	gnome-weather \
-	marker \
-	onboard
-
-echo "[Desktop Entry]
-Name=Fix Touch
-Comment=Fix
-Exec=/home/luca-linux/bin/fix_touch.sh
-Terminal=true
-Type=Application
-Icon=system-reboot
-Categories=GTK;Utility;
-Keywords=Start again;Rotate;
-StartupNotify=true
-Hidden=true
-X-GNOME-Autostart-enabled=true
-X-GNOME-Autostart-Delay=5" | tee ~/.config/autostart/fix_touch.desktop | tee ~/.local/share/applications/fix_touch.desktop
+#echo "[Desktop Entry]
+#Name=Fix Touch
+#Comment=Fix
+#Exec=/home/luca-linux/bin/fix_touch.sh
+#Terminal=true
+#Type=Application
+#Icon=system-reboot
+#Categories=GTK;Utility;
+#Keywords=Start again;Rotate;
+#StartupNotify=true
+#Hidden=true
+#X-GNOME-Autostart-enabled=true
+#X-GNOME-Autostart-Delay=5" | tee ~/.config/autostart/fix_touch.desktop | tee ~/.local/share/applications/fix_touch.desktop
 
 echo "[Desktop Entry]
 Name=Rotate
@@ -57,65 +39,17 @@ Icon=audio-volume-high-symbolic
 Categories=GTK;Utility;
 StartupNotify=true" | tee ~/.local/share/applications/fix_sound.desktop
 
-echo 'Section "Device"
-  Identifier "Intel Graphics"
-  Driver "intel"
-
-  Option "TearFree" "true"
-EndSection' | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
-
 echo '[Desktop Entry]
 Name=Mpv Play
 Comment=play from clipboard
 Exec=/home/luca-linux/bin/mpv_play
 Terminal=false
 Type=Application
-Icon=/home/luca-linux/Syncthing/Conf/Shortcuts/applications/video-play.png
+Icon=mpv
 Categories=GTK;Utility;
-StartupNotify=true' | tee ~/.local/share/applications/fix_sound.desktop
+StartupNotify=true' | tee ~/.local/share/applications/mpv_play.desktop
 
-cd ~/Syncthing/Conf || exit 1
-for ext in *extension.zip; do
-	gnome-extensions install "$ext" --force
-done
-cd - || exit 1
+gsettings set org.gnome.desktop.interface scaling-factor 1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.75
 
-gsettings set org.onboard theme '/usr/share/onboard/themes/Droid.theme'
-gsettings set org.onboard layout '/usr/share/onboard/layouts/Compact.onboard' #Phone.onboard
-gsettings set org.onboard.auto-show enabled true
-gsettings set org.onboard.keyboard audio-feedback-enabled true
-gsettings set org.onboard.keyboard touch-feedback-enabled true
-gsettings set org.onboard.window docking-enabled true
-gsettings set org.onboard.keyboard touch-feedback-size 100
-
-gsettings set org.gnome.desktop.lockdown disable-lock-screen true
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.5
-gsettings set org.gnome.mutter experimental-features "['x11-randr-fractional-scaling']"
-
-echo '<monitors version="2">
-  <configuration>
-    <logicalmonitor>
-      <x>0</x>
-      <y>0</y>
-      <scale>2</scale>
-      <primary>yes</primary>
-      <transform>
-        <rotation>right</rotation>
-        <flipped>no</flipped>
-      </transform>
-      <monitor>
-        <monitorspec>
-          <connector>DSI-1</connector>
-          <vendor>unknown</vendor>
-          <product>unknown</product>
-          <serial>unknown</serial>
-        </monitorspec>
-        <mode>
-          <width>1200</width>
-          <height>1920</height>
-          <rate>60.023551940917969</rate>
-        </mode>
-      </monitor>
-    </logicalmonitor>
-  </configuration>
-</monitors>' | sudo tee /var/lib/gdm/.config/monitors.xml
+sudo dnf repolist | tail -n+2 | cut -d' ' -f1 | xargs -I{} sudo dnf config-manager --set-enabled {}-testing
