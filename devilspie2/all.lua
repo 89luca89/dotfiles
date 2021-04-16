68adb07f6af5925e266526798bcedc0c1568cb2c
89luca89 <luca.dimaio1@gmail.com>
2021-04-16 13:57:03 +0200

    Update keybindings

--- devilspie2/all.lua
+++ devilspie2/all.lua
@@ -1,5 +1,5 @@
 if string.match(get_window_type(), "WINDOW_TYPE_NORMAL") then
-  if (string.find(get_application_name(),"MPV")==nil) then
+  if (string.find(get_application_name(),"mpv")==nil) then
     if (string.find(get_application_name(),"alculator")==nil) then
       center()
       maximize();
