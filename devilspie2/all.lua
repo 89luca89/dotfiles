cc7f1e21dff28fe791ca3aa4728d1435934122e3
89luca89 <luca.dimaio1@gmail.com>
2021-06-20 21:54:47 +0200

    rofi: set onedark theme

--- devilspie2/all.lua
+++
@@ -1,9 +0,0 @@
-if string.match(get_window_type(), "WINDOW_TYPE_NORMAL") then
-  if (string.find(get_application_name(),"mpv")==nil) then
-    if (string.find(get_application_name(),"alculator")==nil) then
-      center()
-      maximize();
-    end
-  end
-  focus();
-end
