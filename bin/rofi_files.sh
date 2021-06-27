d7b6aacadeb42fcaa4828254a8b1b8778a2b3e57
89luca89 <luca.dimaio1@gmail.com>
2021-06-27 12:06:51 +0200

    rofi: merge windows/apps/files in single rofi

--- bin/rofi_files.sh
+++ bin/rofi_files.sh
@@ -6,7 +6,7 @@ CUR_DIR=$PWD
 
 if [ -z $1 ]; then
     # Print formatted listing
-    FILES=$(rg --files "$CUR_DIR") && DIRS=$(rg --files --null "$CUR_DIR" | xargs -0 dirname) && echo "$FILES" "$DIRS" | sort -u
+    rg --files "$CUR_DIR" | sort -u
 else
     xdg-open "$1"
 fi
