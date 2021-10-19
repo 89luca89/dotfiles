c8088ac277912fc564eee6e4abea4a08d64d792c
89luca89 <luca.dimaio1@gmail.com>
2021-10-19 18:59:30 +0200

    improve *buntu support

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,4 +2,18 @@
 
 DIR="$(dirname $0)"
 
-ansible-playbook -i localhost, -c local -K "$DIR"/main.yml --diff $@
+base_deps="git python3 ansible"
+
+if which dnf >/dev/null; then
+	PKG="dnf"
+elif which apt >/dev/null; then
+	PKG="apt"
+fi
+
+for i in $base_deps; do
+	if ! which "$i" >/dev/null; then
+		sudo $PKG install -y "$i"
+	fi
+done
+
+ansible-playbook -i localhost, -c local -K "$DIR"/main.yml --diff $*
