757e6485c5ea4065bbc0e987e881f6882faadea2
89luca89 <luca.dimaio1@gmail.com>
2021-10-23 15:33:47 +0200

    update rasp setup

--- desktop/setup_rasp.sh
+++ desktop/setup_rasp.sh
@@ -1,88 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o errexit
 set -o nounset
-
-apt update
-apt upgrade -y
+set -o pipefail
 
 echo 'max_usb_current=1' | tee -a /boot/config.txt
 
-apt install -y cryptsetup iotop htop iftop tmux vim lm-sensors
+apt update; apt upgrade -y
+apt install -y \
+    cryptsetup
+    htop
+    iftop
+    iotop
+    lm-sensors
+    nginx
+    parallel
+    rclone
+    rsync
+    tmux
+    vim
 
 yes | sensors-detect
 
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 
-echo '#!/bin/sh
-
-        if ! ls /dev/mapper/secret; then
-        cryptsetup luksOpen /dev/sda1 secret --key-file /root/lukskey-disk
-        mount /dev/mapper/secret /usr/local/mnt/Vault
-        cd /root/docker
-        docker-compose down
-        docker-compose up -d
-else
-        if ! df | grep -q Vault; then
-                mount /dev/mapper/secret /usr/local/mnt/Vault
-                cd /root/docker
-                docker-compose down
-                docker-compose up -d
-        fi
-fi' | tee /root/mount.sh
-chmod +x /root/mount.sh
-
 ln -s /usr/local/mnt/Vault/Syncthing/Bin /root/bin
-
-apt install -y stunnel
-ln -sf /bin/bash /bin/rbash
-echo /bin/rbash >>/etc/shells
-
-userdel pi
-
-useradd -s /usr/bin/rbash -U stunnel
-mkdir /home/stunnel
-rsync -av --progress /usr/local/mnt/Vault/Syncthing/Secured/Stunnel-Server /home/stunnel/Stunnel-Server
-chown -R stunnel.stunnel /home/stunnel/
-
-echo '
-[Unit]
-Description=SSL tunnel for network daemons
-After=network.target
-After=syslog.target
-
-[Install]
-WantedBy=multi-user.target
-Alias=stunnel.target
-
-[Service]
-User=stunnel
-Group=stunnel
-Type=forking
-ExecStart=/usr/bin/stunnel /home/stunnel/Stunnel-Server/stunnel.conf
-ExecStop=/usr/bin/killall -9 stunnel
-
-# Give up if ping dont get an answer
-TimeoutSec=600
-
-Restart=always
-PrivateTmp=false' | tee /etc/systemd/system/stunnel-custom.service
-
-pip3 install "glances[all]"
-
-echo '
-[Unit]
-Description = Glances in Web Server Mode
-After = network.target
-
-[Service]
-ExecStart = /usr/local/bin/glances  -w  -t  5 -1
-
-[Install]
-WantedBy = multi-user.target' | tee /etc/systemd/system/glances.service
-
-apt install rclone -y
 ln -s /usr/local/mnt/Vault/Syncthing/Conf/.rclone.conf ~/
 
 pip3 install docker-compose
@@ -91,4 +34,9 @@ systemctl status docker
 cd ~/docker/
 docker compose up -d
 
-crontab /usr/local/mnt/Vault/Syncthing/Conf/crontab.cron
+crontab ~/docker/crontab.backup
+ln -s ~/docker/data/nginx/*.conf /etc/nginx/sites-enabled/
+
+cat ~/docker/hosts >> /etc/hosts
+
+userdel pi
