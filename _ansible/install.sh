aee7cd4223415e1e252f775cc97b5cbcc3b96453
89luca89 <luca.dimaio1@gmail.com>
2021-11-22 18:41:04 +0100

    Support rootless setups

--- _ansible/install.sh
+++ _ansible/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 DIR="$(dirname $0)"
+PATH=$PATH:$HOME/.local/bin
 
-base_deps="git python3 ansible"
+base_deps="git python3"
 
 if command -v dnf >/dev/null; then
 	PKG="dnf"
@@ -10,10 +11,27 @@ elif command -v apt >/dev/null; then
 	PKG="apt"
 fi
 
-for i in $base_deps; do
-	if ! command -v "$i" >/dev/null; then
-		sudo $PKG install -y "$i"
-	fi
-done
+if ! command -v git >/dev/null; then
+	sudo $PKG install -y git
+fi
 
-ansible-playbook -i localhost, -c local --ask-vault-pass -K "$DIR"/main.yml --diff $*
+if ! command -v python3 >/dev/null; then
+	sudo $PKG install -y python3
+fi
+
+if ! command -v pip3 >/dev/null; then
+	sudo $PKG install -y python3-pip
+fi
+
+if ! command -v ansible >/dev/null; then
+	pip3 install ansible==2.9.23
+fi
+
+if groups | grep -Eq "sudo|wheel"; then
+	ansible-playbook -i localhost, -c local --ask-vault-pass -K "$DIR"/main.yml --diff $*
+else
+	echo "Skipping some tags, user is not in sudoers"
+	ansible-playbook -i localhost, -c local --ask-vault-pass \
+		--skip-tags archive,archives,backports,codecs,debloat,grub,intel,virtualization,desktop_packages,flatpak_packages_debloat,powersave,rpmfusion,snapd,storage,sysctl,podman,wifi,zram \
+		"$DIR"/main.yml --diff $*
+fi
