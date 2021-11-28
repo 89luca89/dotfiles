7898c4b91d63e47872f9ce0bc095d91e3d1255f0
89luca89 <luca.dimaio1@gmail.com>
2021-11-28 12:54:35 +0100

    ansible: separate admin tasks in become tag, support install on rootless setups

--- _ansible/install.sh
+++ _ansible/install.sh
@@ -6,7 +6,7 @@ PATH=$PATH:$HOME/.local/bin
 base_deps="git python3"
 
 if command -v dnf >/dev/null; then
-	PKG="dnf"
+	PKG="dnf "
 elif command -v apt >/dev/null; then
 	PKG="apt"
 fi
@@ -15,23 +15,15 @@ if ! command -v git >/dev/null; then
 	sudo $PKG install -y git
 fi
 
-if ! command -v python3 >/dev/null; then
-	sudo $PKG install -y python3
-fi
-
-if ! command -v pip3 >/dev/null; then
-	sudo $PKG install -y python3-pip
-fi
-
 if ! command -v ansible >/dev/null; then
-	pip3 install ansible==2.9.23
+	sudo $PKG install -y ansible
 fi
 
 if groups | grep -Eq "sudo|wheel"; then
 	ansible-playbook -i localhost, -c local --ask-vault-pass -K "$DIR"/main.yml --diff $*
 else
 	echo "Skipping some tags, user is not in sudoers"
-	ansible-playbook -i localhost, -c local --ask-vault-pass \
-		--skip-tags archive,archives,backports,codecs,debloat,grub,intel,virtualization,desktop_packages,flatpak_repo,powersave,rpmfusion,snapd,storage,sysctl,podman,wifi,zram \
+	ansible-playbook -i localhost, --ask-vault-pass \
+		--skip-tags become
 		"$DIR"/main.yml --diff $*
 fi
