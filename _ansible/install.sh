936de08867875c69af2ba916809d8d4f72a6d8bb
89luca89 <luca.dimaio1@gmail.com>
2022-02-17 15:32:28 +0100

    ansible: simplify things

--- _ansible/install.sh
+++ _ansible/install.sh
@@ -3,20 +3,15 @@
 DIR="$(dirname $0)"
 PATH=$PATH:$HOME/.local/bin
 
-base_deps="git python3"
-
 if command -v dnf >/dev/null; then
 	PKG="dnf "
 elif command -v apt >/dev/null; then
 	PKG="apt"
 fi
 
-if ! command -v git >/dev/null; then
-	sudo $PKG install -y git
+if ! command -v git >/dev/null ||
+	 ! command -v ansible >/dev/null; then
+	sudo $PKG install -y git ansible sshpass
 fi
 
-if ! command -v ansible >/dev/null; then
-	pip3 install ansible==2.9.10
-fi
-
-ansible-playbook -i localhost, -c local --ask-vault-pass -K "$DIR"/main.yml --diff $*
+ansible-playbook -i localhost, -c local -K "$DIR"/main.yml --diff $*
