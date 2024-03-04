27b4ef3219d0e9c07a558fe6e1b78768532badc6
Luca Di Maio <luca.dimaio1@gmail.com>
2024-03-04 18:37:38 +0100

    add install.sh script
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+"$(dirname "$0")/bin/.local/bin/setup-dotfiles"
+"$(dirname "$0")/bin/.local/bin/setup-distro"
+"$(dirname "$0")/bin/.local/bin/setup-flatpak"
+"$(dirname "$0")/bin/.local/bin/setup-gnome"
