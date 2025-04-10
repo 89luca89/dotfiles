e4e269c52bb19aeb58d075839058508f76e1a111
Luca Di Maio <luca.dimaio1@gmail.com>
2025-04-10 08:48:00 +0200

    use container setup script
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 "$(dirname "$0")/bin/.local/bin/setup-dotfiles"
 "$(dirname "$0")/bin/.local/bin/setup-distro"
-"$(dirname "$0")/bin/.local/bin/setup-syncthing"
+"$(dirname "$0")/bin/.local/bin/setup-containers"
 "$(dirname "$0")/bin/.local/bin/setup-flatpak"
 "$(dirname "$0")/bin/.local/bin/setup-gnome"
