266497c64e616d9393f0d9a3f83da58d8ff229b7
Luca Di Maio <luca.dimaio1@gmail.com>
2025-10-27 17:53:40 +0100

    cleanup
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-"$(dirname "$0")/bin/.local/bin/setup-dotfiles"
-"$(dirname "$0")/bin/.local/bin/setup-distro"
-"$(dirname "$0")/bin/.local/bin/setup-containers"
-"$(dirname "$0")/bin/.local/bin/setup-flatpak"
-"$(dirname "$0")/bin/.local/bin/setup-gnome"
